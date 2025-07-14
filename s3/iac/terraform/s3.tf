d506a06fbb052ac778c9a30c84656b371db3fcfd
Shikha Singh <shikhasingh4934@gmail.com>
2025-07-14 20:49:29 +0000

    terraform example

--- s3/iac/terraform/s3.tf
+++ s3/iac/terraform/s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "my-tf-s3-bucket" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "my-tf-s3-bucket-sh5465" {
+  bucket = "my-tf-test-bucket-sh5465"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}                                       
+}   
